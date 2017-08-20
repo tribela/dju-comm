e3bf27501df6061f56bb8ff476a9fcc2f61097b2
kjwon15 <kjwonmail@gmail.com>
2017-08-20 10:24:06 +0900

    Setup basic configures

---
+++ Procfile
@@ -0,0 +1,1 @@
+dev: PYTHON_UNBUFFERED=1 DEBUG=on ./manage.py runserver
--- dju_community/settings.py
+++ dju_community/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import environ
+
+root = environ.Path(__file__) - 2
+print(root)
+env = environ.Env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,10 +80,8 @@ WSGI_APPLICATION = 'dju_community.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
+    'extra': env.db('SQLITE_URL', default='sqlite:///db.sqlite3'),
 }
 
 
