fb2a9c9f2150813827549fe3168519cc8b6c8f0a
kjwon15 <kjwonmail@gmail.com>
2017-08-29 18:50:43 +0900

    Make "import_excel" to import excel
    Delete unnecessary DataFile table.

--- classrating/admin.py
+++ classrating/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.conf.urls import url
+from .models import Class, Professor, TimeTable
+from .views import import_excel_file
 
-from .models import Class, Professor, TimeTable, DataFile
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ['year', 'semester', 'title',
+                    'division', 'classification', 'credit', 'professor']
+    list_filter = ['year', 'semester', 'grade', 'credit']
+
+    # TODO: Add link to `import_excel_file` view on admin page.
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                '^import_excel/$',
+                self.admin_site.admin_view(import_excel_file),
+                name='Import excel'
+            ),
+        ]
+        return custom_urls + urls
 
 
-admin.site.register(Class)
+admin.site.register(Class, ClassAdmin)
 admin.site.register(Professor)
 admin.site.register(TimeTable)
-admin.site.register(DataFile)
--- classrating/views.py
+++ classrating/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.admin.views.decorators import staff_member_required
+from .forms import ExcelImportForm
 
-# Create your views here.
+
+@staff_member_required
+def import_excel_file(request):
+    if request.method == 'POST':
+        form = ExcelImportForm(request.POST, request.FILES)
+        if form.is_valid() or True:
+            form.save()
+            # TODO: Change this to admin page.
+            return redirect('/')
+        else:
+            context = {
+                'form': form
+            }
+    else:
+        context = {
+            'form': ExcelImportForm()
+        }
+    return render(request, "form.html", context)
