9c01a4a2b3d6a1d5797ead6beb42e7eeb7f01dca
kjwon15 <kjwonmail@gmail.com>
2017-08-29 20:51:39 +0900

    Use celery to import excel file

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 dev: PYTHON_UNBUFFERED=1 DEBUG=on ./manage.py runserver
 web: uwsgi --master --die-on-term --http :${PORT:-5000} --module dju_community.wsgi
+worker: celery -A dju_community worker -l info
--- classrating/forms.py
+++ classrating/forms.py
@@ -1,8 +1,6 @@
-import re
-import xlrd
-
 from django import forms
-from .models import Class, Professor, TimeTable
+from .models import Class
+from .tasks import import_excel
 
 
 class ExcelImportForm(forms.ModelForm):
@@ -13,90 +11,8 @@ class ExcelImportForm(forms.ModelForm):
         model = Class
         fields = ['year', 'semester', 'excel_file']
 
-    @staticmethod
-    def parse_timetables(class_, rawstring):
-        pattern = re.compile(
-            r'(?!, )((?P<place>.+?):)?'
-            r'(?P<day>[월화수목금토일])\((?P<times>\d+(?:,\d+)*)\)')
-        day_map = {
-            '월': '1',
-            '화': '2',
-            '수': '3',
-            '목': '4',
-            '금': '5',
-            '토': '6',
-            '일': '7',
-        }
-        timetables = []
-        for item in pattern.finditer(rawstring):
-            if item.group('place'):
-                place = item.group('place').strip()
-            day = item.group('day')
-            times = item.group('times')
-            for time in map(int, times.split(',')):
-                timetable, created = TimeTable.objects.get_or_create(
-                    class_to=class_,
-                    day=day_map[day],
-                    time=time)
-                timetable.place = place
-                if created:
-                    timetable.save()
-                timetables.append(timetable)
-
-        return timetables
-
     def save(self):
+        year = self.cleaned_data['year']
+        semester = self.cleaned_data['semester']
         content = self.cleaned_data['excel_file'].read()
-        workbook = xlrd.open_workbook(file_contents=content)
-        sheet = workbook.sheet_by_index(0)
-        for i in range(2, sheet.nrows):
-            row = sheet.row_values(i)
-
-            professor_name = row[5]
-            professor_depart = row[6]
-            professor, created = Professor.objects.get_or_create(
-                name=professor_name, depart=professor_depart)
-            if created:
-                professor.save()
-
-            code = row[1]
-            division = row[3]
-            title = row[2]
-            classification = row[4]
-            try:
-                capacity = int(row[7])
-            except ValueError:
-                capacity = 0
-            university = row[10]
-            department = row[11]
-            major = row[12]
-            category = row[13]
-            try:
-                grade = int(row[14])
-            except ValueError:
-                grade = None
-            try:
-                credit = int(row[16])
-            except ValueError:
-                credit = 0
-
-            class_, created = Class.objects.update_or_create(
-                year=self.cleaned_data['year'],
-                semester=self.cleaned_data['semester'],
-                code=code,
-                division=division,
-                defaults={
-                    'title': title,
-                    'professor': professor,
-                    'classification': classification,
-                    'capacity': capacity,
-                    'university': university,
-                    'department': department,
-                    'major': major,
-                    'category': category,
-                    'grade': grade,
-                    'credit': credit,
-                })
-
-            class_.timetables.set(self.parse_timetables(class_, row[24]))
-            class_.save()
+        import_excel.delay(year, semester, content)
